f47cb364d1bec4967aa2425054fd7ccfcf4adfad
viankaarchery91-sys <viankaarchery91@gmail.com>
2025-12-10 10:51:05 +0200

    day_02

---
+++ day02.py
@@ -0,0 +1,28 @@
+# read in the data
+import sys
+read = sys.stdin.read
+f = open("day02.txt")
+# split function looks at the text in the input file and breaks it up based on a comma
+input_ids = f.read().split(',')
+
+# Now check if every number in the string is repeated (invalid)
+def is_invalid(num):
+    num_length = len(str(num))
+
+    if num_length % 2 == 1:    # Check to see if the number length is odd. For an invalid id both numbers need to be even
+        return False
+    return str(num)[ :num_length // 2] == str(num)[num_length // 2:]
+
+# Finally add the invalid  for each ID
+ans = 0
+
+for id in input_ids:
+    lo, hi = id.split('-')
+    lo = int(lo)
+    hi = int(hi)
+
+for num in range(lo, hi + 1):
+    if is_invalid(num):
+        ans += num
+
+print(ans)
